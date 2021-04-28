6a24e36e04102b9d46efdf65129bb982910e0530
Keriz <guillaume.thivolet@free.fr>
2021-04-28 17:31:15 +0200

    Adapt embed to cryptobrutas

--- src/discord.js
+++ src/discord.js
@@ -23,15 +23,9 @@ function createEmbed(repo, branch, url, commits, size, report) {
     var latest = commits[0];
 
     var embed = new discord.RichEmbed()
-                .setColor(getEmbedColor(report))
-                .setURL(url)
-                .setTitle(size + (size == 1 ? " Commit was " : " Commits were ") + "added to " + repo + " (" + branch + ")")
+                .setColor(0x00BB22)
+                .setTitle(`*cryptobrutas* -\`${commits[0].author.username}\` pushed ${size} ${(size > 1) ? "commit" : "commits"} to ${repo} on branch ${branch}`)
                 .setDescription(getChangeLog(commits, size))
-                .setTimestamp(Date.parse(latest.timestamp));
-
-    if (report.tests.length > 0) {
-        appendTestResults(embed, report);
-    }
 
     return embed;
 }
@@ -40,85 +34,11 @@ function getChangeLog(commits, size) {
     var changelog = "";
 
     for (var i in commits) {
-        if (i > 3) {
-            changelog += `+ ${size - i} more...\n`;
-            break;
-        }
-
         var commit = commits[i];
         var sha = commit.id.substring(0, 6);
-        var message = commit.message.length > MAX_MESSAGE_LENGTH ? (commit.message.substring(0, MAX_MESSAGE_LENGTH) + "..."): commit.message;
-        changelog += `[\`${sha}\`](${commit.url}) ${message} (@${commit.author.username})\n`;
+        var message = commit.message;
+        changelog += `\`${sha}\` ${commit.message} - ${commit.author.username})\n`;
     }
 
     return changelog;
-}
-
-function getEmbedColor(report) {
-    if (report.status === "FAILURE") {
-        return 0xE80000;
-    }
-
-    if (report.tests.length > 0) {
-        var skipped = 0;
-        var failures = 0;
-
-        for (var i in report.tests) {
-            var status = report.tests[i].status;
-            if (status === "SKIPPED") skipped++;
-            if (status === "FAILURE" || status === "ERROR") failures++;
-        }
-
-        if (failures > 0) {
-            return 0xFF6600;
-        }
-        if (skipped > 0) {
-            return 0xFF9900;
-        }
-
-        return 0x00FF00;
-    }
-    else {
-        return 0x00BB22;
-    }
-}
-
-function appendTestResults(embed, report) {
-    var title = false;
-    var passes = 0;
-    var skipped = 0;
-    var failures = [];
-
-    for (var i in report.tests) {
-        var status = report.tests[i].status;
-        if (status === "OK") passes++;
-        else if (status === "SKIPPED") skipped++;
-        else failures.push(report.tests[i].name);
-    }
-
-    var tests = "";
-
-    if (passes > 0) {
-        tests += ` :green_circle: ${passes} Tests passed`;
-    }
-
-    if (skipped > 0) {
-        tests += ` :yellow_circle: ${skipped} Tests were skipped`;
-    }
-
-    if (failures.length > 0) {
-        tests += ` :red_circle: ${failures.length} Tests failed\n`;
-
-        for (var i in failures) {
-            if (i > 2) {
-                tests += `\n+ ${failures.length - i} more...`;
-                break;
-            }
-
-            tests += `\n${parseInt(i) + 1}. \`${failures[i]}\``;
-        }
-    }
-
-    embed.addField("Unit Tests" + (failures.length > 0 ? "": ` (~${report.coverage}% coverage):`), tests);
-    embed.setFooter(`Finished in ${report.time}s`);
 }
